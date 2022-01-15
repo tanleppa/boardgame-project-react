f18ed3b4fe6211ec8918e9310c95fa0b0ecb34f6
Taneli Leppälä <tanleppa@gmail.com>
2022-01-15 20:51:58 +0200

    scroll to top on logo click

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,21 @@ import Logo from '../assets/logo.png'
 import { Link } from "react-router-dom"
 
 const Footer = () => {
+    function scrollToTop() {
+        window.scrollTo(0, 0)
+    }
+
     return (
         <footer>
             <div className="container">
                 <div className="row row__column">
-                    <Link to="/">
+                    <Link to="/" onClick={scrollToTop}>
                         <figure className="footer__logo">
                             <img src={Logo} alt="" className='footer__logo--img'/>
                         </figure>
                     </Link>
                     <div className="footer__list">
-                        <Link to="/" className='footer__link'>Etusivu</Link>
+                        <Link to="/" className='footer__link' onClick={scrollToTop}>Etusivu</Link>
                         <span className='footer__link no-cursor'>Tietoa</span>
                         <Link to="/boardgames" className='footer__link'>Lautapelit</Link>
                         <Link to="/cart" className='footer__link'>Ostoskori</Link>
